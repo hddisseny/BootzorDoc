0dd81e2f258d579c3f19dc4167d475718593beea
hddisseny <hddisseny@gmail.com>
2022-05-09 19:34:08 +0200

    Add old website, it's work =P

--- BootzorDoc/Pages/Error.cshtml.cs
+++ BootzorDoc/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BootzorDoc.Pages
+namespace BlazorComponents.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
