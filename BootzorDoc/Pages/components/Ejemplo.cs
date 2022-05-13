7e9bb96db15c9d141258ca960a81ac27ece0151f
RamónMN <ramon.martinez.nieto@gmail.com>
2022-05-13 20:07:35 +0200

    add pages of the autoform

---
+++ BootzorDoc/Pages/components/Ejemplo.cs
@@ -0,0 +1,9 @@
+﻿namespace BootzorDoc.Pages.components;
+
+public class Ejemplo
+{
+    public int EstoEsUnInt { get; set; }
+    public string EstoEsUnString { get; set; }
+    public bool EstoEsUnBool { get; set; }
+
+}
